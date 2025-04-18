f75311e4165fca3442ab99647a1e82914eab55e4
PraiseKeyz <praiseoluwatobilobaadebayo@gmail.com>
2025-04-18 10:09:50 +0100

    Adjust file extension

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import translateRoutes from './routes/translateRoutes.ts';
+import translateRoutes from './routes/translateRoutes.js';
 
 const app = express();
 const PORT = 3000;
